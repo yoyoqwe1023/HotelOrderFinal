59547d61218bcd904ff2918aca13c2f75ace184a
Tiramisu <noddy55168@gmail.com>
2023-05-04 23:34:12 +0800

    頁面必要JS檔 Views跟Controller初版更新

---
+++ HotelOrderFinal/Controllers/HotelIndustryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelOrderFinal.Controllers
+{
+    public class HotelIndustryController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
