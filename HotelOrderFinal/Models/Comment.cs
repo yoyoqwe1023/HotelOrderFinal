062a3e3726dea37125a3bafe6d6dfea00657b611
Su shun lan <landysu53@gmail.com>
2023-05-05 17:54:54 +0800

    新增

--- HotelOrderFinal/Models/Comment.cs
+++ HotelOrderFinal/Models/Comment.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelOrderFinal.Models
 {
     public partial class Comment
     {
         public int CommentId { get; set; }
+        [DisplayName("日　期")]
         public DateTime? CommentDate { get; set; }
-        public string? MemberId { get; set; }
-        public string? RoomClassId { get; set; }
-        public decimal? CommentPoint { get; set; }
-        public string? CommentDetail { get; set; }
+        [DisplayName("會員編號")]
+        public string MemberId { get; set; }
+        [DisplayName("評　點")]
+        [Required(ErrorMessage = "評點是必填欄位")]
+        public int? CommentPoint { get; set; }
+        [DisplayName("評　價")]
+        public string CommentDetail { get; set; }
 
         public virtual RoomMember? Member { get; set; }
         public virtual RoomClass? RoomClass { get; set; }
