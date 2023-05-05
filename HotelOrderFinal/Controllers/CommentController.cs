e9a8c032909b7d17072f47458629d3f97fb8ddf7
Su shun lan <landysu53@gmail.com>
2023-05-05 17:55:18 +0800

    新增

--- HotelOrderFinal/Controllers/CommentController.cs
+++ HotelOrderFinal/Controllers/CommentController.cs
@@ -8,7 +8,7 @@ namespace HotelOrderFinal.Controllers
         public IActionResult List()
         {
             HotelOrderContext db = new HotelOrderContext();
-            var datas = from c in db.Comment
+            var datas = from c in db.Comments
                         select c;
             //var datas = db.Comments.Include("RoomMembers");
             return View(datas);
