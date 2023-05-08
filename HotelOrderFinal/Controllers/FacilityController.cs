c0356dfb955b3ed54007ec7db2e4333692d068f6
yoyoqwe1023 <yoyoqwe1023@gmail.com>
2023-05-08 11:47:41 +0800

    FacilityController

---
+++ HotelOrderFinal/Controllers/FacilityController.cs
@@ -0,0 +1,49 @@
+﻿using HotelOrderFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelOrderFinal.Controllers
+{
+    public class FacilityController : Controller
+    {
+        private IWebHostEnvironment _enviro;
+        public FacilityController(IWebHostEnvironment p)
+        {
+            _enviro = p;
+        }
+        public IActionResult Index()
+        {
+            HotelOrderContext db = new HotelOrderContext();
+            var datas = from c in db.RoomFacility
+                        select c;
+            return View(datas);            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            HotelOrderContext db = new HotelOrderContext();
+            RoomFacility facility = db.RoomFacility.FirstOrDefault(t => t.FacilityId == id);
+            if (facility == null)
+                return RedirectToAction("Index");
+            return View(facility);
+        }
+        [HttpPost]
+        public IActionResult Edit(CFacilityWrap p)
+        {
+            HotelOrderContext db = new HotelOrderContext();
+            RoomFacility facility = db.RoomFacility.FirstOrDefault(t => t.FacilityId == p.facilityId);
+            if (facility != null)
+            {
+                if (p.photo != null)
+                {
+                    string photoName = Guid.NewGuid().ToString() + ".jpg";
+                    string path = _enviro.WebRootPath + "/image/" + photoName;
+                    p.photo.CopyTo(new FileStream(path, FileMode.Create));
+                    facility.FacilityImage = photoName;
+                }
+                facility.FacilityName = p.FacilityName;             
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HotelOrderFinal/Models/CFacilityWrap.cs
@@ -0,0 +1,37 @@
+﻿namespace HotelOrderFinal.Models
+{
+    public class CFacilityWrap
+    {
+        private RoomFacility _facility;
+
+        public RoomFacility facility
+        {
+            get { return _facility; }
+            set { _facility = value; }
+        }
+
+        public CFacilityWrap()
+        {
+            facility = new RoomFacility();
+        }
+
+        public int facilityId
+        {
+            get { return _facility.FacilityId; }
+            set { _facility.FacilityId = value; }
+        }
+        public string FacilityName
+        {
+            get { return _facility.FacilityName; }
+            set { _facility.FacilityName = value; }
+        }
+        public string? FacilityImage
+        {
+            get { return _facility.FacilityImage; }
+            set { _facility.FacilityImage = value; }
+        }
+       
+        public IFormFile photo { get; set; }
+
+    }
+}
