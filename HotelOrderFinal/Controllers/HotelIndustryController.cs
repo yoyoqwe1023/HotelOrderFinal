d5976b146297c8480b16a066e5e46bc05237535b
noddy55168 <noddy55168@gmail.com>
2023-05-09 16:20:11 +0800

    修改樣式與新增Create,delete

--- HotelOrderFinal/Controllers/HotelIndustryController.cs
+++ HotelOrderFinal/Controllers/HotelIndustryController.cs
@@ -14,16 +14,9 @@ namespace HotelOrderFinal.Controllers
         }
 
         public IActionResult List ( )
-        //現在這個寫法是display全部datas，沒有做分類，要思考一下分類作法
-        //找demo的查詢應該是一個分類方向
-        //0507:可以透過where條件強制做分類處理，現在綁死為1
         {
-            //HotelOrderContext db = new HotelOrderContext ( );
-            //var datas = from HI in db.HotelIndustry
-            //            where HI.HotelRegionId == 1
-            //            select HI;
-            //return View ( datas );
-            return View ();
+
+            return View ( );
         }
 
         public IActionResult Edit ( int? id )
@@ -61,28 +54,28 @@ namespace HotelOrderFinal.Controllers
 
             return RedirectToAction ( "List" );
         }
-
-        //    public string demoObjtoJson ( )
-        //    {
-        //        HotelIndustry db = new HotelIndustry ( )
-        //        {
-        //            HotelId = 1 ,
-        //            HotelName = "華泰蔚藍飯店" ,
-        //            HotelPhone = "(02) 6531-6582" ,
-        //            HotelAddress = "台北市復興南路一段390號" ,
-        //            HotelImage = "524efbc9-7b42-4f36-8b07-4b6581b1600b.jpg" ,
-        //            HotelImageDiscription = "華泰蔚藍飯店照片(128173663.jpg)" ,
-        //            HotelRegionId = 1 ,
-        //        };
-        //        string json = JsonSerializer.Serialize ( db );
-        //        return json;
-        //    }
-        //    public string demoJsontoObj ( )
-        //    {
-        //        string json = demoObjtoJson ( );
-        //        HotelIndustry db = JsonSerializer.Deserialize<HotelIndustry> ( json );
-        //        return db.HotelId + "<br/>" + db.HotelName + "<br/>"+  db.HotelPhone + "<br/>" + db.HotelImage;
-        //    }
-        //}
+        public IActionResult Create ( ) //創建資料
+        {
+            return View ( );
+        }
+        [HttpPost]
+        public IActionResult Create ( HotelIndustry p )
+        {
+            HotelOrderContext db = new HotelOrderContext ( );
+            db.HotelIndustry.Add ( p );
+            db.SaveChanges ( );
+            return RedirectToAction ( "List" );
+        }
+        public IActionResult Delete (int? id )
+        {
+            HotelOrderContext db = new HotelOrderContext ( );
+            HotelIndustry cust = db.HotelIndustry.FirstOrDefault ( t => t.HotelId == id );
+            if ( id != null )
+            {
+                db.HotelIndustry.Remove ( cust );
+                db.SaveChanges ( );
+            }
+            return RedirectToAction ( "List" );
+        }
     }
 }
