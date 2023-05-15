ef09efbb6505dbd118d9ca9c629b77ffca4fc250
Su shun lan <landysu53@gmail.com>
2023-05-15 14:05:21 +0800

    完成評價

--- HotelOrderFinal/Controllers/CommentController.cs
+++ HotelOrderFinal/Controllers/CommentController.cs
@@ -1,25 +1,52 @@
 ﻿using HotelOrderFinal.Models;
+using HotelOrderFinal.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Linq;
 
 namespace HotelOrderFinal.Controllers
 {
     public class CommentController : Controller
     {
-        public IActionResult List()
+        public IHttpContextAccessor _contextAccessor;
+        public CommentController(IHttpContextAccessor contextAccessor)
+        {
+            this._contextAccessor = contextAccessor;
+        }
+        public IActionResult List(CKeywordViewModel vm)
         {
             HotelOrderContext db = new HotelOrderContext();
-            var datas = from c in db.Comment
+            IEnumerable<Comment> datas = null;
+            if (string.IsNullOrEmpty(vm.txtKeyword))
+                datas = from c in db.Comment
+                        orderby c.CommentDate descending
                         select c;
+            else
+                datas = db.Comment
+                    .Where(p => p.CommentDetail.Contains(vm.txtKeyword))
+                    .OrderByDescending(p => p.CommentDate);
             //var datas = db.Comments.Include("RoomMembers");
             return View(datas);
         }
+        public IActionResult ListByMember(string? UserID)
+        {
+            var userId = _contextAccessor.HttpContext.Session.GetString("UserID");
+            HotelOrderContext db = new HotelOrderContext();
+            IEnumerable<Comment> member = db.Comment.Where(t => t.MemberId == userId);
+            if (member == null)
+                return RedirectToAction("List");
+            return View(member);
+        }
         public IActionResult Create()
+        //public IActionResult Create(int? userid)
         {
             if (HttpContext.Session.GetString("UserID") == null)
             {
                 return RedirectToAction("Login", "Member");
             }
             return View();
+            //ViewBag.MemberId = userid;
+            //return View(userid);
         }
         [HttpPost]
         public IActionResult Create(Comment p)
@@ -28,6 +55,38 @@ namespace HotelOrderFinal.Controllers
             db.Comment.Add(p);
             db.SaveChanges();
             return RedirectToAction("List");
+        }
+        public IActionResult Edit(int? id)
+        {
+            HotelOrderContext db = new HotelOrderContext();
+            Comment comment = db.Comment.FirstOrDefault(t => t.CommentId == id);
+            if (comment == null)
+                return RedirectToAction("ListByMember");
+            return View(comment);
+        }
+        [HttpPost]
+        public IActionResult Edit(Comment p)
+        {
+            HotelOrderContext db = new HotelOrderContext();
+            Comment comment = db.Comment.FirstOrDefault(t => t.CommentId == p.CommentId);
+            if (comment != null)
+            {
+                comment.CommentDetail = p.CommentDetail;
+                db.SaveChanges();
+            }
+            return RedirectToAction("ListByMember");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            HotelOrderContext db = new HotelOrderContext();
+            Comment comment = db.Comment.FirstOrDefault(t => t.CommentId == id);
+            if (comment != null)
+            {
+                db.Comment.Remove(comment);
+                db.SaveChanges();
+            }
+            return RedirectToAction("ListByMember");
         }
     }
 }
--- HotelOrderFinal/wwwroot/js/comment/start.js
+++ HotelOrderFinal/wwwroot/js/comment/start.js
@@ -1,58 +1,64 @@
-//製作一個星星評分
-var divStars = document.getElementById('stars');
-var score = document.getElementById('score');
-var attitude = [1, 2, 3, 4, 5];
-var starNum = -1; //記錄當前第幾顆星星被點擊
-var starArray = Array.from(divStars.children); //星星數組
+console.log("xyz")
+////製作一個星星評分
+//var divStars = document.getElementById('stars');
+//var score = document.getElementById('score');
+//const commentPoint = document.getElementById('CommentPoint'); 
+//var attitude = [1, 2, 3, 4, 5];
+//var starNum = -1; //記錄當前第幾顆星星被點擊
+//var starArray = Array.from(divStars.children); //星星數組
+//console.log("start");
+////滑鼠移入
+//divStars.onmouseover = function (e) {
+//    console.log("over");
+//    if (e.target.tagName === "IMG") { //事件源是圖片
+//        //把滑鼠移動到的星星替換圖片
+//        e.target.src = "../image/comment/shining.png";
+//        //把滑鼠移動到的星星之前的星星替換圖片
+//        var prev = e.target.previousElementSibling;
+//        while (prev) {
+//            prev.src = "../image/comment/shining.png";
+//            prev = prev.previousElementSibling;
+//        }
+//        //把滑鼠移動到的星星之後的星星替換圖片
+//        var next = e.target.nextElementSibling;
+//        while (next) { //把滑鼠移動到的星星之後的星星替換圖片
+//            next.src = "../image/comment/empty.png";
+//            next = next.nextElementSibling;
+//        }
+//        var index = starArray.indexOf(e.target); //找到滑鼠移動到的星星的序號
+//        score.innerHTML = attitude[index]; //顯示對應的點數
+//        commentPoint.value = 12; //attitude[index]; //顯示對應的點數
+//    }
+//}
+////滑鼠點擊
+//divStars.onclick = function (e) {
+//    if (e.target.tagName === "IMG") {
+//        //記錄當前點擊的星星序號
+//        starNum = starArray.indexOf(e.target);
+//        /*        CommentPoint = Number(starNum) + 1;*/
+//        /*        console.log(CommentPoint)*/
+//    }
+//}
 
-//滑鼠移入
-divStars.onmouseover = function (e) {
-    if (e.target.tagName === "IMG") { //事件源是圖片
-        //把滑鼠移動到的星星替換圖片
-        e.target.src = "../image/comment/shining.png";
-        //把滑鼠移動到的星星之前的星星替換圖片
-        var prev = e.target.previousElementSibling;
-        while (prev) {
-            prev.src = "../image/comment/shining.png";
-            prev = prev.previousElementSibling;
-        }
-        //把滑鼠移動到的星星之後的星星替換圖片
-        var next = e.target.nextElementSibling;
-        while (next) { //把滑鼠移動到的星星之後的星星替換圖片
-            next.src = "../image/comment/empty.png";
-            next = next.nextElementSibling;
-        }
-        var index = starArray.indexOf(e.target); //找到滑鼠移動到的星星的序號
-        score.innerHTML = attitude[index]; //顯示對應的點數
-    }
-}
-//滑鼠點擊
-divStars.onclick = function (e) {
-    if (e.target.tagName === "IMG") {
-        //記錄當前點擊的星星序號
-        starNum = starArray.indexOf(e.target);
-        /*        CommentPoint = Number(starNum) + 1;*/
-        /*        console.log(CommentPoint)*/
-    }
-}
+////滑鼠移出
+//divStars.onmouseout = function (e) {
+//    if (starNum !== -1) { //滑鼠點擊事件發生，將評分固定在點擊的星星上
+//        for (var i = 0; i < divStars.children.length; i++) {
+//            if (i <= starNum) {
+//                divStars.children[i].src = "../image/comment/shining.png"
+//            } else {
+//                divStars.children[i].src = "../image/comment/empty.png";
+//            }
+//        }
+//        score.innerHTML = attitude[starNum]; //顯示對應的點數
+//        commentPoint.value = attitude[starNum]; //顯示對應的點數
+//    } else {
+//        for (var i = 0; i < divStars.children.length; i++) {
+//            divStars.children[i].src = "../image/comment/empty.png";
+//        }
+//        score.innerHTML = 0; //不顯示點數
+//        commentPoint.value = 0;
+//    }
+//}
 
-//滑鼠移出
-divStars.onmouseout = function (e) {
-    if (starNum !== -1) { //滑鼠點擊事件發生，將評分固定在點擊的星星上
-        for (var i = 0; i < divStars.children.length; i++) {
-            if (i <= starNum) {
-                divStars.children[i].src = "../image/comment/shining.png"
-            } else {
-                divStars.children[i].src = "../image/comment/empty.png";
-            }
-        }
-        score.innerHTML = attitude[starNum]; //顯示對應的點數
-    } else {
-        for (var i = 0; i < divStars.children.length; i++) {
-            divStars.children[i].src = "../image/comment/empty.png";
-        }
-        score.innerHTML = 0; //不顯示點數
-    }
-}
-
-/*document.forms['a'].b.value = CommentPoint;*/
+///*document.forms['a'].b.value = CommentPoint;*/
