06e68e6aa1061d59d85e02df60c4d03b1da92804
yoyoqwe1023 <yoyoqwe1023@gmail.com>
2023-05-22 20:42:03 +0800

    後臺管理 Room和Member

---
+++ HotelOrderFinal/Controllers/MemberMangeController.cs
@@ -0,0 +1,48 @@
+﻿using HotelOrderFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HotelOrderFinal.Controllers
+{
+    public class MemberMangeController : Controller
+    {
+        //public IActionResult List()
+        //{
+        //    ViewBag.SelectedItemContent = "會員管理的內容";
+        //    HotelOrderContext db = new HotelOrderContext();
+        //    var datas = from c in db.RoomMember
+        //                select c;
+        //    return View(datas);
+        //}
+
+        public IActionResult List(int pageNumber = 1, int pageSize = 10)
+        {
+            ViewBag.SelectedItemContent = "會員管理的內容";
+            HotelOrderContext db = new HotelOrderContext();
+
+            var totalItems = db.RoomMember.Count();
+            var totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            var pagedData = db.RoomMember
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(pagedData);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+
+    }
+}
