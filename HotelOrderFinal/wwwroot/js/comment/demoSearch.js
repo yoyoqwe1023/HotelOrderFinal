895551d3beea8bc953fa6a520009b96c67b95be7
Su shun lan <landysu53@gmail.com>
2023-05-26 15:43:40 +0800

    更改評論關鍵字搜尋Demo內容

--- HotelOrderFinal/wwwroot/js/comment/demoSearch.js
+++ HotelOrderFinal/wwwroot/js/comment/demoSearch.js
@@ -2,7 +2,7 @@ function demoSearch() {
     // 取得表單元素和欄位元素
     var txtKeyword = document.getElementById("txtKeyword");
     // 設定 Demo 關鍵字
-    txtKeyword.value = "房間";
+    txtKeyword.value = "喜歡";
     // 提交表單
     form.submit();
 }
